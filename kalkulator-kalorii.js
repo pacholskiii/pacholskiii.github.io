26fc87b453eac1757ed645f7f7f8d5cec068141f
pacholskiii <szymon.pacholek24@gmail.com>
2023-12-23 20:50:39 +0100

    zmiany

--- kalkulator-kalorii.js
+++ kalkulator-kalorii.js
@@ -5,25 +5,42 @@ let masa = document.querySelector("#masa");
 let aktywnosc = document.querySelector("#aktywnosc");
 let cel = document.querySelector("#cel");
 
+let wyniki = document.querySelector(".wyniki")
 let oblicz = document.querySelector("#oblicz");
+
+let results = 
 
 oblicz.addEventListener("click", () => {
    
-    if(isNaN(wiek.value) || isNaN(wzrost.value) || isNaN(masa.value)){
-        oblicz.innerHTML = "Podaj poprawne wartości";
+    if(isNaN(parseFloat(wiek.value)) || isNaN(parseFloat(wzrost.value)) || isNaN(parseFloat(masa.value))){
+        oblicz.innerHTML = "Podaj poprawne wartości!";
         oblicz.style.fontSize = "18px";
+        
+        setTimeout(() => {
+            oblicz.innerHTML = "Oblicz";
+        }, 3000);
+       
+       
     }
     else{
-        let plecValue = plec.value;
-        let wiekValue = wiek.value;
-        let wzrostValue = wzrost.value;
-        let masaValue = masa.value;
-        let aktywnoscValue = aktywnosc.value;
-        let celValue = cel.value;
-
-        console.log(celValue);
-
+          getValues();
+          writeAnswers();
     }
    
 
 });
+
+function getValues(){
+    let plecValue = plec.value;
+    let wiekValue = wiek.value;
+    let wzrostValue = wzrost.value;
+    let masaValue = masa.value;
+    let aktywnoscValue = aktywnosc.value;
+    let celValue = cel.value;
+}
+
+function writeAnswers(){
+    wyniki.style.display = "block";
+
+
+}
