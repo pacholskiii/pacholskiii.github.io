c1ba4872b3f0e85d15c86ab225d9bcf0d16a522e
pacholskiii <szymon.pacholek24@gmail.com>
2024-01-16 12:46:07 +0100

    Sekcja 'poczatki treningu' w sekcji 'jak-zaczac'

---
+++ jak-zaczac.js
@@ -0,0 +1,28 @@
+let btnArrow = document.querySelectorAll(".btn-arrow");
+let modals = document.querySelectorAll(".modal-container");
+let items = document.querySelectorAll(".item");
+let value;
+let btnClose = document.querySelectorAll(".btn-close");
+
+
+
+
+
+console.log();
+
+btnArrow.forEach((btn, index) => {
+    btn.addEventListener("click", () =>{
+        modals[index].classList.add("active")
+      
+    });
+});
+
+btnClose.forEach(btn => {
+    btn.addEventListener("click", () =>{
+        console.log('ada');
+        modals.forEach((modal) =>{
+            modal.classList.remove("active");
+        })
+    })
+    
+});
