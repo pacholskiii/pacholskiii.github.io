8ce283714dc6547edb4604f432bb3df3f63c3655
pacholskiii <szymon.pacholek24@gmail.com>
2024-03-05 18:17:42 +0100

    Finished training plans

--- plany.js
+++ plany.js
@@ -22,16 +22,38 @@ var swiper = new Swiper(".mySwiper", {
       },
   });
 
+// Card click
 
 
+let planItems = document.querySelectorAll(".accordion-body");
+let accordionContainer = document.querySelectorAll(".accordion-table-container");
+
+let slides = document.querySelectorAll(".plan-desc");
+let container = document.querySelector(".inner-body-wave");
+let accordion = document.querySelectorAll(".accordion");
+let ch_d = document.querySelectorAll(".ch-d")
 
 
-
-let clickableDesc = document.querySelectorAll(".plan-desc");
-let slides = document.querySelectorAll(".plans")
-
-clickableDesc.forEach((desc, index) => {
-    desc.addEventListener("click", () => {
-        console.log(index);
-    });
+slides.forEach((slide, index) => {
+    slide.addEventListener("click", () => {
+        accordion[index].style.display = "block";
+        
+    })
 }); 
+
+ch_d.forEach((chevron, index) => {
+  chevron.addEventListener("click", () => {
+
+    console.log(index);
+   
+    if( accordionContainer[index].classList.contains("collapsed")){
+      accordionContainer[index].classList.toggle("collapsed")
+      chevron.classList.remove("rotate");
+    }
+    else{
+      accordionContainer[index].classList.toggle("collapsed");
+      chevron.classList.add("rotate");
+    }
+  });
+});
+
